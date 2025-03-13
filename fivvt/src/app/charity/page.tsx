1057407dd2d2cad77e0d92f9c95698e68158cab0
Robocotik <alexander.starkin@gmail.com>
2025-03-13 22:06:52 +0300

    [feat] pages: add charity

---
+++ fivvt/src/app/charity/page.tsx
@@ -0,0 +1,7 @@
+import React, {FC} from 'react';
+
+const Page: FC = () => {
+  return <div>fwefwef</div>;
+};
+
+export default Page;
--- fivvt/src/app/page.tsx
+++ fivvt/src/app/page.tsx
@@ -1,16 +1,9 @@
-import {Header} from '@/components/Header';
-import { Footer } from '@/components/Footer';
-import {HeaderUsecase} from '@/components/Header/Header.usecase';
-import {FooterUsecase} from '@/components/Footer/Footer.usecase';
-import {DropDownActions} from '@/components/DropdownActions';
-import { DropDownActionsUsecase } from '@/components/DropdownActions/DropdownActions.usecase';
+
 
 export default function Home() {
   return (
     <div>
-      <Header {...HeaderUsecase} />
-      <DropDownActions {...DropDownActionsUsecase} />
-      <Footer {...FooterUsecase} />
+      main page
     </div>
   );
 }
