9023eaf20313abaf40ec2923913b6b0a297a1284
Robocotik <alexander.starkin@gmail.com>
2025-03-13 15:50:38 +0300

    [feat] button: update

--- fivvt/src/app/page.tsx
+++ fivvt/src/app/page.tsx
@@ -1,11 +1,12 @@
 import {Header} from '@/components/Header';
-import { HeaderUsecase } from '@/components/Header/Header.usecase';
+import { Footer } from '@/components/Footer';
+import {HeaderUsecase} from '@/components/Header/Header.usecase';
+import {FooterUsecase} from '@/components/Footer/Footer.usecase';
 export default function Home() {
   return (
     <div>
       <Header {...HeaderUsecase} />
-      
-      {/* // <Footer /> */}
+      <Footer {...FooterUsecase} />
     </div>
   );
 }
