56150e22158fbee6f311d386ed2bf06a6b952dd3
Robocotik <alexander.starkin@gmail.com>
2025-03-13 23:43:06 +0300

    [feat] page: update charity

--- fivvt/src/app/charity/page.tsx
+++ fivvt/src/app/charity/page.tsx
@@ -1,7 +1,21 @@
+'use client';
 import React, {FC} from 'react';
+import {PageLayout} from '@/layouts/PageLayout';
+import {Card} from '@/components/Card';
+import {
+  CardQRUsecase,
+  CardInformationUsecase,
+} from '@/components/Card/Card.usecase';
 
 const Page: FC = () => {
-  return <div>fwefwef</div>;
+  return (
+    <PageLayout title='Платежные реквизиты'>
+      <div className='w-full  gap-[min(10%,5rem)] flex flex-nowrap'>
+        <Card {...CardInformationUsecase} />
+        <Card {...CardQRUsecase} />
+      </div>
+    </PageLayout>
+  );
 };
 
 export default Page;
--- fivvt/src/layouts/PageLayout/index.tsx
+++ fivvt/src/layouts/PageLayout/index.tsx
@@ -1,9 +1,17 @@
 import type {FC} from 'react';
 import type Props from './PageLayout.props';
 
-export const PageLayout: FC<Props> = ({className, children, ...props}) => {
+export const PageLayout: FC<Props> = ({
+  className,
+  title,
+  children,
+  ...props
+}) => {
   return (
-    <div className={`${className} w-full flex flex-col px-inline-main_px`} {...props}>
+    <div
+      className={`${className} w-full flex flex-col gap-8 items-center px-inline-main_px py-8`}
+      {...props}>
+      {title && <p className='font-bold text-3xl'>{title}</p>}
       {children}
     </div>
   );
