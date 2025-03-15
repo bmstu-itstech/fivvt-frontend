9b6ae1cff0593a53cb67e722078300321945522d
Robocotik <alexander.starkin@gmail.com>
2025-03-16 02:31:23 +0300

    [feat] icon: add arrow

---
+++ fivvt/src/components/icons/ArrowRightIcon/index.tsx
@@ -0,0 +1,17 @@
+import type {FC} from 'react';
+import type Props from './ArrowRightIcon.props';
+import Image from 'next/image';
+import src from '@/assets/arrow_right.svg';
+
+export const ArrowRightIcon: FC<Props> = ({className, ...props}) => {
+  return (
+    <Image
+      className={`${className ?? ''} `}
+      {...props}
+      src={src}
+      alt='ArrowRightIcon'
+      width={40}
+      height={40}
+    />
+  );
+};
