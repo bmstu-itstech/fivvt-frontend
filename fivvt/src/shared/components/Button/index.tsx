ed1fc2a849069289a1287baae4db2ff6e4362bc6
Robocotik <alexander.starkin@gmail.com>
2025-03-16 20:50:14 +0300

    [feat] compoenents: update button

--- fivvt/src/shared/components/Button/index.tsx
+++ fivvt/src/shared/components/Button/index.tsx
@@ -4,10 +4,10 @@ import type Props from './Button.props';
 export const Button: FC<Props> = ({className, children, icon, ...props}) => {
   return (
     <button
-      className={`${className} hover:scale-95 hover:shadow duration-300 transition-all min-w-4 min-h-4 h-full max-h-10 px-6 rounded-xl w-fit items-center justify-center flex gap-2 cursor-pointer`}
+      className={`${className} text-xs hover:scale-95 hover:shadow duration-300 transition-all min-w-4 min-h-4 h-full max-h-10 px-6 rounded-xl w-fit items-center justify-center flex gap-2 cursor-pointer`}
       {...props}>
       {icon}
-      <p className='text-nowrap w-full text-xs'>{children}</p>
+      <p className='text-nowrap w-full text-inherit'>{children}</p>
     </button>
   );
 };
