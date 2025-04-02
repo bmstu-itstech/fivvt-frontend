de5fa4868d057abfd23d11fbe3ad6963e405506d
Robocotik <alexander.starkin@gmail.com>
2025-04-02 13:43:50 +0300

    [feat] api: rewrite request to api

--- fivvt/src/shared/apis/boards/getBoard.tsx
+++ fivvt/src/shared/apis/boards/getBoard.tsx
@@ -4,7 +4,7 @@ import {IBoard} from '@/domain/entitites/board';
 const board: (id: number) => Promise<IBoard> = async (id)  => {
   try {
     const response = await client.get(
-      `${process.env.NEXT_PUBLIC_API_URL}api/v0/board/${id}`,
+      `${process.env.NEXT_PUBLIC_API_URL}api/v0/board/${id}/`,
     );
     return response.data;
   } catch (error) {
