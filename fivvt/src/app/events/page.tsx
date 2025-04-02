7717b21124b38f68c8a3c2ceb2eadd846335762b
Robocotik <alexander.starkin@gmail.com>
2025-04-02 14:57:10 +0300

    [feat] events: add not done events

--- fivvt/src/app/events/page.tsx
+++ fivvt/src/app/events/page.tsx
@@ -11,30 +11,61 @@ const Page: FC = () => {
   }, [isLoading]);
   return (
     <PageLayout title='Мероприятия'>
-      <div className='w-full flex flex-wrap'>
-        {isLoading || !data ? (
-          <>
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-            <EventCardSkeleton />
-          </>
-        ) : (
-          data.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className='px-2 py-2 w-1/4 flex justify-center items-center'>
-                <EventCard className='w-full h-full' data={item} />
-              </div>
-            );
-          })
-        )}
+      <div className='w-full flex flex-col gap-2'>
+        <p className='text-2xl font-medium'>Предстоящие:</p>
+        <div className='w-full flex flex-wrap'>
+          {isLoading || !data ? (
+            <>
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+            </>
+          ) : (
+            data.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className='px-2 py-2 w-1/4 flex justify-center items-center'>
+                  <EventCard className='w-full h-full' data={item} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+      <div className='w-full flex flex-col gap-2'>
+        <p className='text-2xl font-medium'>Прошедшие:</p>
+        <div className='w-full flex flex-wrap'>
+          {isLoading || !data ? (
+            <>
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+            </>
+          ) : (
+            data.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className='px-2 py-2 w-1/4 flex justify-center items-center'>
+                  <EventCard className='w-full h-full' data={item} />
+                </div>
+              );
+            })
+          )}
+        </div>
       </div>
     </PageLayout>
   );
