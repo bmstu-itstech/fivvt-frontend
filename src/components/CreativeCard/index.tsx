c37ab1cfb406a5c7e3b36734113567ce303e2ee5
Robocotik <alexander.starkin@gmail.com>
2025-04-23 23:43:35 +0300

    [feat] page: update style of creative page

--- src/components/CreativeCard/index.tsx
+++ src/components/CreativeCard/index.tsx
@@ -15,12 +15,17 @@ export const CreativeCard: FC<Props> = ({data, className}) => {
             alt='author photo'
             height={600}
             width={600}
-            className=' animate-appear-start-block-800 w-1/2 aspect-square rounded-2xl object-center object-cover pb-4 pe-4 border-b-2 border-e-2 border-amber-500'
+            className=' animate-appear-start-block-800 w-1/2 aspect-square rounded-2xl object-center object-cover  border-b-4 border-e-4 border-amber-400'
           />
           <Divider isVertical className='bg-gray-200' />
-          <div className='flex flex-col gap-4 justify-center items-center w-full max-w-1/2 animate-appear-end-block-800'>
+          <div className='flex relative flex-col gap-4 justify-center items-center w-full max-w-1/2 animate-appear-end-block-800'>
             <p className='text-2xl'>{data.author.name}</p>
             <p className='text-gray-500 text-xl'>{data.author.post}</p>
+            <div className='absolute top-0 left-0 p-2 -z-10 flex w-full h-full bg-amber-400'>
+              <div className='bg-white z-40 w-full h-full self-center mx-auto'></div>
+              <div className='bg-white absolute left-0 top-8 w-full h-[calc(100%-4rem)]'></div>
+              <div className='bg-white absolute top-0 right-8  w-[calc(100%-4rem)] h-full'></div>
+            </div>
           </div>
         </div>
         <div className='w-full flex flex-wrap'>
