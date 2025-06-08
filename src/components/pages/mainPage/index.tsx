47273ce5c380664144290bb1bc7c3f5d68298d1b
Robocotik <alexander.starkin@gmail.com>
2025-06-08 13:51:44 +0300

    [feat] change text of 1st main screen

--- src/components/pages/mainPage/index.tsx
+++ src/components/pages/mainPage/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import {Button} from '@/shared/components/Button';
 import {useRouter} from 'next/navigation';
 import war from '@/assets/war_bg_1.jpg';
+import {MainPageUsecase} from './MainPage.usecase';
 
 export const MainPage: FC = () => {
   const router = useRouter();
@@ -20,9 +21,7 @@ export const MainPage: FC = () => {
         <div
           data-twe-animation-on-scroll='repeat'
           className='flex flex-col justify-center gap-8 items-center z-10  text-white w-full max-w-1/2  animate-appear-start-block-1500'>
-          <p className='text-2xl text-center max-w-1/2'>
-            Фонд инвалидов войн и военной травмы
-          </p>
+          <p className='text-2xl text-center max-w-1/2'>{MainPageUsecase.title}</p>
           <Button
             onClick={() => {
               router.push('/charity');
@@ -36,20 +35,14 @@ export const MainPage: FC = () => {
           className=' h-[clamp(16rem,54vh,22rem)] animate-appear-end-block-1500  bg-transparent border-b-2 border-l-2
          text-white flex flex-col gap-4 justify-center items-start
            border-white/20 rounded-bl-2xl py-4 ps-8 pe-inline-main_px 
-           text-pretty w-fit max-w-1/2'>
-          <p className='pb-2'>
-            Фонд действует с 1995 года. Его участниками в настоящее время
-            являются около 500 инвалидов-ветеранов боевых действий как на
-            территории Республики Афганистан, так и других горячих точках
-            бывшего Советского Союза, в том числе при проведении
-            контртеррористической операции в Чеченской Республике.
-          </p>
-          <p>
+           text-pretty w-fit max-w-1/2 whitespace-pre-wrap'>
+          <p className='pb-2'>{MainPageUsecase.text_desc}</p>
+          {/* <p>
             Сегодня это почти 60 инвалидов 1 группы и около 300 - 2 группы.
             Остальная часть их составляет инвалиды 3 группы и ветераны боевых
             действий, своим посильным вкладов оказывающих помощь собратьям по
             оружию.
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
