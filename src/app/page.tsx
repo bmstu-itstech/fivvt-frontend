f3ebcb5032d562998b8ff92451ddd9ec5c76634a
Robocotik <alexander.starkin@gmail.com>
2025-06-08 14:38:22 +0300

    [feat] add mainRootPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 import {MainPage} from '@/components/pages/mainPage';
+import { MainRootPage } from '@/components/pages/mainRootPage';
 
 export default function Home() {
   return (
     <div className='w-full'>
       <MainPage />
-      {/* <HistoryPage /> */}
+      <MainRootPage />
     </div>
   );
 }
---
+++ src/components/pages/mainRootPage/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+import type {FC} from 'react';
+import Image from 'next/image';
+import {Button} from '@/shared/components/Button';
+import {useRouter} from 'next/navigation';
+import war from '@/assets/war_bg_1.jpg';
+import {MainRootPageUsecase} from './MainRootPage.usecase';
+import {PageLayout} from '@/layouts/PageLayout';
+import {Divider} from '@/shared/components/Divider';
+export const MainRootPage: FC = () => {
+  return (
+    <PageLayout>
+      <div className='flex w-full h-full'>
+        <div className='w-full max-w-2/3 flex flex-col p-2 h-full justify-between items-center'>
+          <div className='flex flex-col h-full  gap-4 w-full items-center'>
+            <p className='text-2xl w-full truncate text-center'>{MainRootPageUsecase.title}</p>
+            <Divider className=' bg-black' />
+          </div>
+          <div className='w-full h-fit pt-4'>
+            <p className='line-clamp-10 text-ellipsis whitespace-pre-wrap'>
+              {MainRootPageUsecase.text_desc}
+            </p>
+          </div>
+        </div>
+        <Image
+          src={MainRootPageUsecase.photo}
+          alt='member'
+          width={800}
+          height={800}
+          className='object-center object-cover p-2 h-full rounded-2xl aspect-square w-1/3 max-w-1/3 cursor-pointer'
+        />
+      </div>
+    </PageLayout>
+  );
+};
