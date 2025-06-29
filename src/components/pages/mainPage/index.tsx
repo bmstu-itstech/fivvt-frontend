c482310ec563b844640dd68632583a03b0e7a9da
Robocotik <alexander.starkin@gmail.com>
2025-06-29 18:38:01 +0300

    [feat] adapt mainPage to mobile

--- src/components/pages/mainPage/index.tsx
+++ src/components/pages/mainPage/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import type {FC} from 'react';
+import {useCallback, type FC} from 'react';
 import Image from 'next/image';
 import {Button} from '@/shared/components/Button';
 import {useRouter} from 'next/navigation';
@@ -8,8 +8,11 @@ import {MainPageUsecase} from './MainPage.usecase';
 
 export const MainPage: FC = () => {
   const router = useRouter();
+  const goToCharity = useCallback(() => {
+    router.push('/charity');
+  }, [router]);
   return (
-    <div className='flex flex-col relative w-full h-dvh bg-gradient-to-b from-transparent to-black/90 from-60%'>
+    <div className='flex flex-col relative w-full lg:justify-start h-dvh bg-gradient-to-b from-transparent to-black/90 from-60%'>
       <Image
         src={war}
         alt='war'
@@ -17,29 +20,27 @@ export const MainPage: FC = () => {
         height={1080}
         className='z-0 absolute top-0 left-0  object-center object-cover brightness-20 h-full w-full'
       />
-      <div className='z-10 flex w-full justify-between overflow-hidden max-w-maxContainer mx-auto'>
+      <div className='z-10 flex lg:flex-row flex-col w-full justify-between px-inline-main_px_mobile lg:px-inline-main_px overflow-hidden max-w-maxContainer mx-auto'>
         <div
           data-aos='fade-right'
           data-aos-duration='1000'
-          className='flex flex-col justify-center gap-8 items-center z-10  text-white w-full max-w-1/2 '>
-          <p className='text-2xl text-center max-w-1/2'>{MainPageUsecase.title}</p>
+          className='flex flex-col mt-12 lg:mt-0 justify-center gap-8 items-center z-10  text-white w-full lg:max-w-1/2 '>
+          <p className='text-3xl lg:text-2xl text-center text-pretty max-w-5/6 lg:max-w-1/2'>{MainPageUsecase.title}</p>
           <Button
-            onClick={() => {
-              router.push('/charity');
-            }}
-            className='border-white/60 animate-pulse-custom border-2 !h-32 hover:bg-green_main hover:text-gray_light hover:border-gray_light duration-200 transition-all'>
+            onClick={goToCharity}
+            className='border-white/60 animate-pulse-custom border-2 !h-40 lg:!h-32 hover:bg-green_main hover:text-gray_light hover:border-gray_light duration-200 transition-all'>
             Поддержать
           </Button>
         </div>
 
         <div
-          className=' h-[clamp(16rem,54vh,22rem)]  bg-transparent border-b-2 border-l-2
+          className=' h-[clamp(16rem,54vh,30rem)] lg:mt-0 mt-12  bg-transparent border-t-2 lg:border-b-2 lg:border-l-2
          text-white flex flex-col gap-4 justify-center items-start
-           border-white/20 rounded-bl-2xl py-4 ps-8 pe-inline-main_px 
-           text-pretty w-fit max-w-1/2 whitespace-pre-wrap'
+           lg:border-white/20 rounded-bl-2xl py-4 ps-8 pe-inline-main_px 
+           text-pretty w-fit lg:max-w-1/2 whitespace-pre-wrap'
           data-aos='fade-left'
           data-aos-duration='1000'>
-          <p className='pb-2'>{MainPageUsecase.text_desc}</p>
+          <p className='lg:pb-2 pt-2'>{MainPageUsecase.text_desc}</p>
         </div>
       </div>
     </div>
